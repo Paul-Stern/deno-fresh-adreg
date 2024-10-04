e2a78435e44bcd9811364be6ff337e7e0af8b846
Paul Stern <paulstern45@gmail.com>
2024-10-04 09:40:45 +0300

    routes: dota2api: rename func

--- routes/api/dota2/matches/[id].tsx
+++ routes/api/dota2/matches/[id].tsx
@@ -1,5 +1,5 @@
 import { PageProps } from "$fresh/server.ts";
 
-export default function Greet(props: PageProps) {
+export default function MatchId(props: PageProps) {
    return <div>Dota 2 match id is: {props.params.id}</div>; 
 }
