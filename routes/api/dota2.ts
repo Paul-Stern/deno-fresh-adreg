fc9ae1676564f4813da06648d54cea6810b2acf5
Paul Stern <paulstern45@gmail.com>
2024-10-10 14:38:44 +0300

    api: dota2: see patch

--- routes/api/dota2.ts
+++ routes/api/dota2.ts
@@ -2,5 +2,5 @@ import { FreshContext } from "$fresh/server.ts";
 import { PageProps } from "$fresh/server.ts";
 
 // export const handler = (_req: Request, _ctx: FreshContext, props: PageProps): Response => {
-//    return <div>Dota 2 match id is: {props.params.name}</div>; 
+//    return <div>Dota 2 match id is: {props.params.name}</div>;
 // }
--- routes/api/dota2/matches/[id].tsx
+++ routes/api/dota2/matches/[id].tsx
@@ -1,5 +1,26 @@
-import { PageProps } from "$fresh/server.ts";
+import { PageProps, Handlers } from "$fresh/server.ts";
+
+interface Match {
+   match_id: number;
+   start_time: number;
+   duration: number;
+   radiant_team_id: number;
+   radiant_score: number;
+   dire_team_id: number;
+   dire_score: number;
+   radiant_win: boolean;
+}
 
 export default function MatchId(props: PageProps) {
-   return <div>Dota 2 match id is: {props.params.id}</div>; 
+   return <div>Dota 2 match id is: {props.params.id}</div>;
+   // const matchResponse = fetch(`https://api.opendota.com/api/matches/${props.params.id}`);
+   // const data = matchResponse.json();
+   // console.log(data);
+   // return <div>{JSON.stringify(matchResponse)}</div>
 }
+
+/*
+export const handler = async (_req: Request, _ctx: Handlers.Context, props: PageProps) => {
+   const matchResponse = await fetch(`https://api.opendota.com/api/matches/${props.params.id}`);
+}
+   */
